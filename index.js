71597a9fdb71c244cd0ec91dc8edb18d4c89f6cb
amazing-hashmap-2071 <haim11293@aol.com>
2019-05-05 19:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   return kittens.pop();
 }
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
